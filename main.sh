29bd14504eee4c9f667155cf17296f761a89fd49
Victor Coss <gtaxl@gtaxl.net>
2015-12-24 00:27:01 -0500

    Removed checkers that don't exist YET from the main script. Working on the other checker types soon.

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# GTAXLnet Uptime Monitor - main.sh Version: 1.00
+# GTAXLnet Uptime Monitor - main.sh Version: 1.01
 # This main code checks if this monitor has Internet against CloudFlare and OpenDNS anycast systems
 # and if the monitor does have Internet will execute all the individual checker type scripts
 # A remote backup monitor will check the Internet on this monitor and will kick in if this monitor goes down
@@ -7,17 +7,15 @@
 # This script will run the uptime checkers in 1 minute intervals for high accuracy.
 # TODO: Compute given uptime for a check in a given month, I can do this by dividing current minute tallies
 # by the amount of minutes in a given month to compute a percentage of uptime.
-# By: Victor Coss (GTAXL) vic@likeacoss.com DEC/21/2015
+# By: Victor Coss (GTAXL) vic@likeacoss.com DEC/24/2015
 
 date=$(date)
 testnet1=$(ping 208.67.222.222 -c 4 -i 0.3 -W 1 | grep "transmitted" | awk '{print $4}')
 testnet2=$(ping 198.41.214.162 -c 4 -i 0.3 -W 1 | grep "transmitted" | awk '{print $4}')
 
 runcheckers() { 
-	/home/gtaxl/uptimemon/tcp-port.sh >> debug.txt 2>&1
-	/home/gtaxl/uptimemon/http.sh >> debug.txt 2>&1
-	/home/gtaxl/uptimemon/smtp.sh >> debug.txt 2>&1
-	/home/gtaxl/uptimemon/localservices.sh >> debug.txt 2>&1
+	/home/gtaxl/uptimemon/http.sh > /dev/null 2>&1 &
+	/home/gtaxl/uptimemon/smtp.sh > /dev/null 2>&1 &
 }
 
 if [ "$testnet1" = "4" ]; then
